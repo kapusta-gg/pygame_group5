40a23ad025d5116797df4d64c13485d39c5b7427
kapusta-gg <bogdashka04@gmail.com>
2024-12-07 17:42:03 +0300

    Добавили текст в MainZone

--- models/zone.py
+++ models/zone.py
@@ -6,8 +6,15 @@ class MainZone:
         self.main_zone_hitbox = pygame.rect.Rect(100, 200, 800, 600)
         self.main_zone_color = pygame.color.Color((0, 255, 0))
 
+        #Создание текста
+        self.font = pygame.font.Font(None, 25)
+        self.text = self.font.render("Основное поле", True, (0, 255, 0))
+
     def draw(self, screen):
         pygame.draw.rect(screen, self.main_zone_color, self.main_zone_hitbox, width=2)
+
+        #Отображение текста
+        screen.blit(self.text, (500, 500))
 
 
 class DeadZone:
